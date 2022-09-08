6820f5b21d27e0372575376cb05bfab3bf828ff1
RodrigoBange <rodrigo99@live.nl>
2022-09-08 17:32:51 +0200

    Assignment03
    Java assignment 03 Week 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,75 +6,45 @@ public class Main {
         // Create a new Scanner object for input
         Scanner scanner = new Scanner(System.in);
 
-        // Display course question
-        System.out.println("Enter course name: ");
-
-        // Save the user input to name
-        String courseName = scanner.nextLine();
-
-        // Display number of student question
-        System.out.println("Enter number of students: ");
-
-        // Save the user input to age
+        // Ask for group size
+        System.out.println("Please enter the size of your group and press [ENTER]: ");
         int nrOfStudents = scanner.nextInt();
 
-        // Create arrays for studentNames and studentGrades
-        String[] studentNames = new String[nrOfStudents];
-        int[] studentGrades = new int[nrOfStudents];
+        // Display group size
+        System.out.println("Group size: " + nrOfStudents);
 
-        // Ask for each students' name and save to array
-        for (int i = 0; i < nrOfStudents; i++) {
-            System.out.println("Enter name of student " + (i + 1) + ": ");
-            String studentName = scanner.next();
-            studentNames[i] = studentName;
-        }
+        // Create array of Student objects
+        Student[] students = new Student[nrOfStudents];
 
-        // Ask for each students' grades and save to array
-        for (int i = 0; i < nrOfStudents; i++) {
-           System.out.println("Enter grade of " + studentNames[i] + ": ");
-           studentGrades[i] = scanner.nextInt();
-        }
-
-        // Calculate the average of all student grades
-        System.out.println("Average grade: " + String.format("%.1f", calculateAverageGrade(studentGrades)));
-
-        // Get student number with the highest grade and display result
-        int maxGradeStudentId = findStudentWithMaxGrade(studentGrades);
-        System.out.println("Student " + studentNames[maxGradeStudentId] + " has maximum grade: " + studentGrades[maxGradeStudentId]);
-
-        // Display all results
+        // Ask for each student name and add to array
         for (int i = 0; i < nrOfStudents; i++){
-            System.out.println("Grade for student " + studentNames[i] + " (course " + courseName + "): " + studentGrades[i]);
-        }
-    }
+            System.out.println("Please enter the name of student #" + (i+1) + " and press [ENTER]: ");
+            String studentName = scanner.next();
 
-    private static double calculateAverageGrade(int[] grades) {
-        // Default value
-        int sum = 0;
-
-        // Add up all grades
-        for (int grade : grades) {
-            sum += grade;
+            students[i] = new Student(studentName);
         }
 
-        // Return average of all grades
-        return (double)sum / grades.length;
-    }
+        // Display all student names
+        for (int i = 0; i < students.length; i++){
+            System.out.println("Student #" + (i+1) + ": " + students[i].name);
+        }
 
-    private static int findStudentWithMaxGrade(int[] grades) {
-        // Default values
-        int maximumGrade = 0;
-        int studentId = 0;
+        // Ask for each student if present
+        for (int i = 0; i < students.length; i++){
+            System.out.println("Is Student #" + (i+1) + "(" + students[i].name + ") present? [Y/N + ENTER]: ");
+            String answer = scanner.next();
 
-        // Find the student number with the maximum grade
-        for (int i = 0; i < grades.length; i++){
-            if (grades[i] > maximumGrade){
-                maximumGrade = grades[i];
-                studentId = i;
+            if (answer.equals("Y") || answer.equals("y")){
+                students[i].present = true;
+            }
+            else if (answer.equals("N") || answer.equals("n")){
+                students[i].present = false;
             }
         }
 
-        // Return id of student
-        return studentId;
+        // Display student presence
+        for (int i = 0; i < students.length; i++){
+            System.out.println("Student #" + (i+1) + ": " + students[i].name + "     | Present: " + students[i].present);
+        }
     }
 }
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Student {
+    String name;
+    boolean present;
+
+    public Student(String name){
+        this.name = name;
+    }
+}
