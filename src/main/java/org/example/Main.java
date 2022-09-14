5d9b6bada7a6783cd16f15367540427f2e89aea7
RodrigoBange <rodrigo99@live.nl>
2022-09-14 10:19:22 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,22 +3,5 @@ import java.util.Scanner; // Import Scanner class
 
 public class Main {
     public static void main(String[] args) {
-        // Create a new Scanner object for input
-        Scanner scanner = new Scanner(System.in);
-
-        // Display name question
-        System.out.println("Enter your name: ");
-
-        // Save the user input to name
-        String name = scanner.nextLine();
-
-        // Display age question
-        System.out.println("Enter your age: ");
-
-        // Save the user input to age
-        int age = scanner.nextInt();
-
-        // Display final result
-        System.out.println("Your name is: " + name + ". " + "Your age is: " + age + ".");
     }
 }
